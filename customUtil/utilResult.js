765ed20d212890dc4da67f700682f313f3b01757
soo man choi <freending@gmail.com>
2018-05-10 20:06:27 +0900

    update : result type format change. result type change module function to module variables.

--- customUtil/utilResult.js
+++ customUtil/utilResult.js
@@ -2,6 +2,11 @@
  * 5월 9, 2018 freend : 최초작성.
  * return json formate
  */
-module.exports.doGetJsonData = function (isProcess, data) {
-    return {isProcess: isProcess, isResult:data};
+module.exports.resultObj = {
+    //작업 결과가 재대로 이뤄졌는지 알려주는 부분.
+    isProcess: false,
+    //클라이언트에서 보여주는 메세지.
+    msg: "",
+    //클라이언트에 들어가는 데이터
+    isData:{}
 };
