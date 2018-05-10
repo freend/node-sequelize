529c8c3777112681dbe5002246cb7f94d14bf7fe
soo man choi <freending@gmail.com>
2018-05-10 20:08:48 +0900

    update : change result format

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -1,11 +1,18 @@
 "use stricts";
 const models = require('../models');
-const dateUtil = require('../customUtil/utilDataTime');
+const dateUtil = require('../customUtil/utilDateTime');
 const resultData = require('../customUtil/utilResult');
 
 module.exports = function (app) {
     app.get('/user/sample', function (req, res) {
         console.log("path : " + req.route.path + ", sample");
+        var sample = Object.create(resultData.resultObj);
+        sample.msg = "aa";
+        console.log("first : " + sample.msg);
+        sample = Object.create(resultData.resultObj);
+        for (var idx in sample) {
+            console.log(idx + ", " + sample[idx]);
+        }
         /*models.userInfo.findAll().then(function (value) {
             res.json(value);
         });*/
@@ -29,28 +36,25 @@ module.exports = function (app) {
             /**
              * 5월 9, 2018 return data인 value는 const로 와서 수정이 불가능 하다 따라서 별도의 obj를 만들어서
              * 필요한 값만 넘겨줬다.
-             * 여기 부분은 추후 개발이 더 필요한 부분이다.
              * @type {{id, name, accessKey: *|accessKey|{type, field}|string, state}}
              */
             const resultValue = {id: value.id, name: value.name, accessKey:value.accessKey, state:value.state};
-            var isData = {msg: "정상적으로 처리되었습니다.",
+            var isData = Object.create(resultData.resultObj);
+            isData.isProcess = true;
+            isData.msg = res.__("isComplete");
+            isData.isData = resultValue;
+            /*var isData = {msg: res.__("isComplete"),
                     isData: resultValue
-                };
-                res.json(resultData.doGetJsonData(true, isData));
+                };*/
+            res.json(isData);
         })
         .catch(function (value) {
-            /**
-             * error part.
-             * "original": {
-                "code": "ER_BAD_NULL_ERROR",
-                "errno": 1048,
-                "sqlState": "23000",
-                "sqlMessage": "Column 'user_id' cannot be null",
-                "sql": "INSERT INTO `USER_INFO` (`user_seq`,`user_access_key`,`user_id`,`user_password`,`user_name`,`user_state`,`user_sign_date`,`user_last_access`) VALUES (DEFAULT,'b24120f0-5372-11e8-a041-b7df88fadb9b',NULL,'chltnaks','freend choi','U',1525861271.678,1525861271.678);"
-            }
-             여기서 필요부분을 서버로 전달하고 변경된 값을 클라이언트로 보내도록 하자.
-             */
-            res.json(resultData.doGetJsonData(false, value));
+            //TODO-freend : 여기는 추후 로그로 만들어 줄 예정입니다.
+            console.log("ERROR : " + value.original.sql);
+            var isData = Object.create(resultData.resultObj);
+            isData.msg = res.__("notComplete");
+            isData.isProcess = false;
+            res.json(isData);
         });
     })
 };
