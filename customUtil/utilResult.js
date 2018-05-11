1c2186c740917e195f75171a9384db128f51d986
soo man choi <freending@gmail.com>
2018-05-11 20:28:32 +0900

    update : update result form name change

--- customUtil/utilResult.js
+++ customUtil/utilResult.js
@@ -2,7 +2,7 @@
  * 5월 9, 2018 freend : 최초작성.
  * return json formate
  */
-module.exports.resultObj = {
+module.exports.resultForm = {
     //작업 결과가 재대로 이뤄졌는지 알려주는 부분.
     isProcess: false,
     //클라이언트에서 보여주는 메세지.
